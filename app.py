751d44f775a4d1f9cdb11a5afadb3f56bfc42dc6
ryannoone10 <151958588+ryannoone10@users.noreply.github.com>
2024-12-01 20:29:01 +0000

    edited HTML

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    powerString = str(mymath.power(3,8))
-    return f'Hello IS3313. This was edited in Github. The answer is: {powerString}'
+    powerString = str(mymath.power(4,3))
+    return f'Hello IS3313.<br/> fixed <br/> This was edited in Github. The answer is: {powerString}'
 
 if __name__ == '__main__':
   app.run()
